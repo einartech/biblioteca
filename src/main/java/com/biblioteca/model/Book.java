4c370453581f53a037e905b27f9fdd3529f3eb08
einartech <imalovelyghost13@gmail.com>
2025-03-27 12:46:26 +0100

    fix-List authors

--- src/main/java/com/biblioteca/model/Book.java
+++ src/main/java/com/biblioteca/model/Book.java
@@ -4,18 +4,17 @@ import java.util.List;
 
 public class Book {
 
-    //PROPIEDADESD DE LA ENTIDAD
+    // PROPIEDADESD DE LA ENTIDAD
     private int id;
     private String title;
-    private List author;
+    private List<String> author;
     private String description;
     private int isbn;
-    private List gender;
+    private List<String> gender;
     private int pages;
 
-
-//CONSTRUCTOR DE LA ENTIDAD
-    public Book(String title, List author, String description, int isbn, List gender, int pages){
+    // CONSTRUCTOR DE LA ENTIDAD
+    public Book(String title, List<String> author, String description, int isbn, List<String> gender, int pages) {
         this.title = title;
         this.author = author;
         this.description = description;
@@ -24,53 +23,63 @@ public class Book {
         this.pages = pages;
     }
 
-    //GETTERS DE LA ENTIDAD
+    // GETTERS DE LA ENTIDAD
     public int getId() {
         return this.id;
     }
+
     public String getTitle() {
         return this.title;
     }
+
     public List getAuthor() {
         return this.author;
     }
+
     public String getDescription() {
         return this.description;
     }
+
     public int getIsbn() {
         return this.isbn;
     }
+
     public List getGender() {
         return this.gender;
     }
+
     public int getPages() {
         return this.pages;
     }
 
+    // SETTERS DE LA ENTIDAD
 
-    //SETTERS DE LA ENTIDAD
-
-     public void  setId(int id) {
+    public void setId(int id) {
         this.id = id;
     }
-    public void  setTitle(String title) {
+
+    public void setTitle(String title) {
         this.title = title;
     }
-    public void setAuthor(List author) {
+
+    public void setAuthor(List<String> author) {
         this.author = author;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
-    public void  setIsbn(int isbn) {
+
+    public void setIsbn(int isbn) {
         this.isbn = isbn;
     }
-    public void setGender(List gender) {
+
+    public void setGender(List<String> gender) {
         this.gender = gender;
     }
-    public void  setPages(int pages) {
+
+    public void setPages(int pages) {
         this.pages = pages;
     }
 
 }
-
