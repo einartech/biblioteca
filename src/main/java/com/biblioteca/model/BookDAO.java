dfc784d0a193044028489dc75a66d863545dce17
edmimi <mirisaor02@gmail.com>
2025-03-27 15:36:30 +0100

    dao

--- src/main/java/com/biblioteca/model/BookDAO.java
+++ src/main/java/com/biblioteca/model/BookDAO.java
@@ -1,5 +1,26 @@
 package com.biblioteca.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+
+import com.biblioteca.config.DBManager;
+
 public class BookDAO {
+
+    private PreparedStatement stmn;
+    private Connection connection;
+
+    public void createBook(Book book){
+        try {
+            connection = DBManager.initConnection();
+            String sql = "INSERT INTO books (title, author, description, isbn, gender, pages) VALUES (?,?,?,?,?,?)";
+            stmn = connection.prepareStatement(sql);
+            stmn.setString(1, book.getTitle());
+            Array authorArray = conn.createArrayOf("text", book.getAuthor().toArray());
+            stmt.setArray(2, authorArray);
+
+        }
+    }
     
 }
